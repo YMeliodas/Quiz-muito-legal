56481c4ff0f44168fdcc75bebeaf3266269725dc
Vertexy <86753662+YMeliodas@users.noreply.github.com>
2021-12-14 12:17:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var resposta1 = prompt ("Quando foi o primeiro mundial de League Of Legends?")
+
+//Verifica se a resposta é diferente (!=) de "2011", se for diferente repete a rotina
+while (resposta1 != "2011"){
+    //abre a tela interativa para poder colocar a senha novamente, ele está salvando o valor que ele recebe do prompt, na variavel resposta
+resposta1 =  prompt ("Quando foi o primeiro mundial de League Of Legends?")
+}
+
+var resposta = prompt ("Qual time ganhou o mundial em 2011")
+
+while (resposta != "Fnatic"){
+resposta =  prompt ("Qual time ganhou o mundial em 2011")
+}
+
+var resposta = prompt ("Quem o jukes mandou para matar o kha'zix pelo menos?")
+
+while (resposta != "Yoda"){
+resposta =  prompt ("Quem o jukes mandou para matar o kha'zix pelo menos?")
+}
+
+var resposta = prompt ("'Solta o filho da puta pra eu ...' O que o jukes queria fazer?")
+
+while (resposta != "Dar um tiro na cabeça dele caralho"){
+resposta =  prompt ("'Solta o filho da puta pra eu ...' O que o jukes queria fazer?")
+}
+
+var resposta = prompt ("Quando jukes dançou a musica Dança do Créu,com qual personagem ele estava?")
+
+while (resposta != "Gangplank"){
+resposta =  prompt ("Quando jukes dançou a musica Dança do Créu,com qual personagem ele estava?")
+}
